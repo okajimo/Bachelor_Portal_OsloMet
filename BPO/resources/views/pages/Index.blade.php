802f0d4e07167499a63126a2a1820fd9cb58dafe
Computer\ltayn <tfury@hotmail.com>
2018-01-23 21:03:07 +0100

    merg test

--- BPO/resources/views/pages/Index.blade.php
+++ BPO/resources/views/pages/Index.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    Index
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus fuga facilis eius perspiciatis, eligendi vel rerum reiciendis sit nisi voluptate autem, dicta sint explicabo sapiente nulla ut odio labore.</p>
 @endsection
