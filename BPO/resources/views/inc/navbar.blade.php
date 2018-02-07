c773959a8452b0e1f6080c249bb6eb841ddce5dd
Ivo <s305516@stud.hioa.no>
2018-02-07 18:32:14 +0100

    Jobbet med fargen på navbare, fremdeles under maintanenc

--- BPO/resources/views/inc/navbar.blade.php
+++ BPO/resources/views/inc/navbar.blade.php
@@ -1,23 +1,23 @@
 <nav class="navbar navbar-inverse">
     <div class="container">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">{{config('app.name', 'BPO')}}</a>
-    </div>
-    <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li><a href="/informasjon">Informasjon</a></li>
-            <li><a href="/prosjektforslag">Prosjektforslag</a></li>
-            <li><a href="/tidligere_prosjekter">Tidligere Prosjekter</a></li>
-            <li><a href="/grupper">Grupper</a></li>
-            <li><a href="/kontakt_info">Kontakt</a></li>
-        </ul>
-    </div><!--/.nav-collapse -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">{{config('app.name', 'BPO')}}</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/informasjon">Informasjon</a></li>
+                <li><a href="/prosjektforslag">Prosjektforslag</a></li>
+                <li><a href="/tidligere_prosjekter">Tidligere Prosjekter</a></li>
+                <li><a href="/grupper">Grupper</a></li>
+                <li><a href="/kontakt_info">Kontakt</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
     </div>
 </nav>
 
