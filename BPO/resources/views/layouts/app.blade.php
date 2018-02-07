1fa26ddbb82d9902bf2d6f34a49c29cdbf7f8496
Ivo <s305516@stud.hioa.no>
2018-02-08 00:26:28 +0100

    Første forslag til Navbar ferdig, vil muligens bli endret litt etter inloggingen er satt inn. Icon er også oppdatert

--- BPO/resources/views/layouts/app.blade.php
+++ BPO/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://student.hioa.no/hioa-theme/images/favicon.ico" rel="Shortcut Icon">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>{{config('app.name', 'BPO')}}</title>
 </head>
